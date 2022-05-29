9d771fde3563fb5f840c865b1a874d3158edfd4c
Axel Hippolite <54190267+AxelHippolite@users.noreply.github.com>
2022-05-30 00:29:52 +0200

    Read Binary File

--- headers/file_reader.h
+++ headers/file_reader.h
@@ -2,8 +2,11 @@
 #define FILE_READER_DOT_H
 #include<stdio.h>
 #include<stdlib.h>
+#include<limits.h>
 
 int getLen(FILE *ptr);
+int getSize(FILE *ptr);
 int occCalculator(FILE *ptr, int nb_letters, char *occ, int *freq);
+char *getBits(FILE *bin, int len);
 
 #endif
